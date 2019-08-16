4936127311299338a0ef34d7b17c27035d432b85
Chris Wysocki <christopher.wysocki@gmail.com>
2019-08-16 14:33:50 -0700

    added tests for authorized and unauthorized get / attempts

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,15 @@
 const app = require('../src/app');
 
 describe('App', () => {
-	it('GET / responds with 200 containing "Hello, world!"', () => {
+	it('Does not allow unauthorized access to the site', () => {
 		return supertest(app)
 			.get('/')
+			.expect(401);
+	});
+	it('GET / responds with 200 containing "Hello, bookmarks!"', () => {
+		return supertest(app)
+			.get('/')
+			.set('Authorization', 'Bearer b476ec9a-22d8-4382-969a-064b208823de')
 			.expect(200, 'Hello, bookmarks!');
 	});
 });
