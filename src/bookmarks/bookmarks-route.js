89044e278d144895df062deb072b2425be7760c3
Chris Wysocki <christopher.wysocki@gmail.com>
2019-08-16 16:47:55 -0700

    Added functionallity and tests for GET /bookmarks POST /bookmarks GET /bookmarks/:id DELETE /bookmarks/:id

--- src/bookmarks/bookmarks-route.js
+++ src/bookmarks/bookmarks-route.js
@@ -17,15 +17,79 @@ function handleGetBookmarks(req, res) {
 	res.json(bookmarks);
 }
 
-function handlePostNewBookmark(req, res) {}
+function handlePostNewBookmark(req, res) {
+	const { title, url, description, rating } = req.body;
+
+	if (!title) {
+		logger.error('Title is required');
+		return res.status(400).send('Invalid data, bookmark must have title');
+	}
+
+	if (!url) {
+		logger.error('url is required');
+		return res.status(400).send('Invalid data, bookmark must have url');
+	}
+
+	if (!description) {
+		logger.error('description is required');
+		return res.status(400).send('Invalid data, bookmark must have description');
+	}
+
+	if (!rating) {
+		logger.error('rating is required');
+		return res.status(400).send('Invalid data, bookmark must have rating');
+	}
+
+	const id = uuid();
+	const newBookmark = {
+		id,
+		title,
+		url,
+		description,
+		rating
+	};
+
+	bookmarks.push(newBookmark);
+
+	logger.info(`Bookmark with id ${id} created`);
+
+	res.status(201)
+		.location(`http://localhost:8000/bookmarks/${id}`)
+		.json(newBookmark);
+}
 
 bookmarksRouter
 	.route('/bookmarks/:id')
 	.get(handleGetBookmarkWithId)
 	.delete(handleDeleteBookmarkWithId);
 
-function handleGetBookmarkWithId(req, res) {}
+function handleGetBookmarkWithId(req, res) {
+	const { id } = req.params;
+	const bookmark = bookmarks.find(bookmark => bookmark.id == id);
 
-function handleDeleteBookmarkWithId(req, res) {}
+	if (!bookmark) {
+		logger.error(`Bookmark with id ${id} not found`);
+		return res.status(404).send('Bookmark Not Found');
+	}
+
+	res.json(bookmark);
+}
+
+function handleDeleteBookmarkWithId(req, res) {
+	const { id } = req.params;
+
+	const bookmarkIndex = bookmarks.findIndex(bm => bm.id == id);
+
+	if (bookmarkIndex === -1) {
+		logger.error(`Could not find bookmark with id ${id}`);
+		return res.status(404).send('bookmark not found');
+	}
+
+	bookmarks.splice(bookmarkIndex, 1);
+
+	logger.info(`Deleted bookmark with id ${id}`);
+
+	res.status(204).end();
+}
 
 module.exports = bookmarksRouter;
--- test/app.spec.js
+++ test/app.spec.js
@@ -31,4 +31,72 @@ describe('/bookmarks', () => {
 				done();
 			});
 	});
+	it('POST /bookmarks responds with 400 if no title given', done => {
+		chai.request(app)
+			.post('/bookmarks')
+			.set('Authorization', 'Bearer b476ec9a-22d8-4382-969a-064b208823de')
+			.end((err, res) => {
+				res.should.have.status(400);
+				done();
+			});
+	});
+	it('POST /bookmarks should respond with 201 if new bookmark is added correctly', done => {
+		chai.request(app)
+			.post('/bookmarks')
+			.set('Authorization', 'Bearer b476ec9a-22d8-4382-969a-064b208823de')
+			.send({
+				'title': 'book',
+				'url': 'https://www.amazon.com',
+				'description': "'the book of amazon'",
+				'rating': 5
+			})
+			.end((err, res) => {
+				res.should.have.status(201);
+				done();
+			});
+	});
+});
+
+describe('/bookmarks/:id', () => {
+	describe('GET ', () => {
+		it('GET /bookmarks/:id should respond with 404 if the bookmark id doesnt exist', done => {
+			chai.request(app)
+				.get('/bookmarks/eoirhegtiuhe')
+				.set('Authorization', 'Bearer b476ec9a-22d8-4382-969a-064b208823de')
+				.end((err, res) => {
+					res.should.have.status(404);
+					done();
+				});
+		});
+		it('GET /bookmarks/:id should respond with 200 and the bookmark object', done => {
+			chai.request(app)
+				.get('/bookmarks/1')
+				.set('Authorization', 'Bearer b476ec9a-22d8-4382-969a-064b208823de')
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					done();
+				});
+		});
+	});
+	describe('DELETE ', () => {
+		it('DELETE /bookmarks/:id should respond with 404 if the bookmark id doesnt exist', done => {
+			chai.request(app)
+				.delete('/bookmarks/eoirhegtiuhe')
+				.set('Authorization', 'Bearer b476ec9a-22d8-4382-969a-064b208823de')
+				.end((err, res) => {
+					res.should.have.status(404);
+					done();
+				});
+		});
+		it('DELETE /bookmarks/:id should respond with status 204 if the bookmark is successfully deleted', done => {
+			chai.request(app)
+				.delete('/bookmarks/1')
+				.set('Authorization', 'Bearer b476ec9a-22d8-4382-969a-064b208823de')
+				.end((err, res) => {
+					res.should.have.status(204);
+					done();
+				});
+		});
+	});
 });
